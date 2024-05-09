307b00603308b19dc570342300bdfe41d1cb641d
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-05-10 01:33:38 +0600

    project started

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+
+const Main = () => {
+    return (
+        <div>
+            {/* navbar */}
+            <Outlet />
+            {/* footer */}
+            
+        </div>
+    );
+};
+
+export default Main;
